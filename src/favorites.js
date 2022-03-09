3fe31bdcf1134f56dfa7753c42df77ad30569559
William Liu <williamliu626@gmail.com>
2022-03-09 11:47:40 -0500

    set up db.json server

--- src/favorites.js
+++ src/favorites.js
@@ -1,8 +1,10 @@
-let object = {}
 
 function clickFav (coin) {
     let button = document.querySelector('#button')
-    button.addEventListener('click', e => {
+    button.addEventListener('click', async e => {
+        const data = await sendData()
+        let object = {}
+        let favRow = document.querySelector('#favrows')
         let tr = document.createElement('tr')
         let tdName = document.createElement('td')
         let tdPrice = document.createElement('td')
@@ -12,7 +14,29 @@ function clickFav (coin) {
         tdPrice.textContent = coin.querySelector('.price').textContent
         tdHour.textContent = coin.querySelector('.percentdiff').textContent
         tr.append(tdName, tdPrice, tdHour)
+        console.log(tr)
+        favRow.appendChild(tr)
+        console.log(favRow)
     
     })
 
 }
+
+async function sendData() {
+    const cgData = {name: "ethereum", current_price: 3000, price_change_percentage_24h: 2.39}
+    try {
+        const response = await fetch('http://localhost:3000/favorites', {
+            method: 'POST', 
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(cgData),
+        })
+        const data = await response.json()
+        console.log('Success:', cgData);
+    } catch(error) {
+        console.error('Error:', error);
+        }
+}
+
+
